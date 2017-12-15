fc8430faf9b23773e4c13c9f95da27fdefd023ad
Joel Edwards <joeledwards@gmail.com>
2017-12-15 10:38:15 -0700

    feat: added utilities to test following a CouchDB

---
+++ follow.js
@@ -0,0 +1,43 @@
+require('log-a-log')
+
+const axios = require('axios')
+const follow = require('follow')
+
+const db = 'https://replicate.npmjs.com/registry'
+const context = 10
+const limit = 10
+const options = {
+  db,
+  include_docs: false
+}
+
+const getSequence = () => {
+  return axios.get(`${db}/_changes?since=now&limit=1`)
+  .then(({data}) => data.last_seq)
+}
+
+const follower = (limit) => {
+  let count = 0
+  return (error, change) => {
+    count++
+
+    if (error) {
+      console.error(`Error in CouchDB follower:`, error)
+    } else {
+      console.log(`[change-${count}]`, JSON.stringify(change, null, 2))
+    }
+
+    // Only process up to the limit
+    if (limit > 0 && count >= limit) {
+      process.exit(0)
+    }
+  }
+}
+
+getSequence()
+.then(sequence => {
+  const since = sequence - context
+  console.log(`Following from sequence ${since}`)
+  follow({...options, since}, follower(limit))
+})
+
---
+++ seq.js
@@ -0,0 +1,36 @@
+require('log-a-log')
+
+const axios = require('axios')
+const durations = require('durations')
+const request = require('request')
+
+const url = `https://replicate.npmjs.com/registry/_changes?since=now&limit=1`
+const options = {
+  validateStatus: status => true
+}
+
+const watch = durations.stopwatch().start()
+
+function axiosGet() {
+  axios
+  .get(url, options)
+  .then(resp => {
+    watch.stop()
+    const {status, data} = resp
+    console.log(`[${status}] (${watch})`, data)
+  })
+  .catch(console.error)
+}
+
+function requestGet() {
+  request.get(url, (err, {statusCode}, body) => {
+    if (err) {
+      bole.error('Failed to fetch last sequence from leader:', err)
+    } else if (statusCode === 200) {
+      const seq = Number.parseInt(JSON.parse(body).last_seq)
+      console.log(seq)
+    }
+  })
+}
+
+requestGet()
